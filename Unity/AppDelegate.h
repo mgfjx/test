7e6f989d37c59ec935fcfc298dbcba8d18541fdc
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2016-07-25 08:51:45 +0800

    Initial Commit

---
+++ Unity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Unity
+//
+//  Created by mgfjx on 16/7/25.
+//  Copyright © 2016年 XXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
